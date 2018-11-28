5d706c6e58c2bd8d8f39eb83023b5fdb87dc3ba7
krushndayshmookh <krushndayshmookh@gmail.com>
2018-11-28 19:29:21 +0530

    aded semicolons

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var express = require('express')
-var path = require('path')
+var express = require('express');
+var path = require('path');
 
-var app = express()
-var port = 3000
+var app = express();
+var port = 3000;
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 
 // -----
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // -----
 
 
 
 app.get('/', function (req, res) {
-    res.render('homepage')
-})
+    res.render('homepage');
+});
 
 // app.get('/red', function (req, res) {
 //     res.send("Hello red!")
 // })
 
 app.listen(port, function () {
-    console.log('App listening on port ' + port + '...')
-})
+    console.log('App listening on port ' + port + '...');
+});
