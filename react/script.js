ddae1db4876b4d213a8ee0c74041bed937ba6764
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-10 20:21:02 +0530

    new components and states and lifecycle and oneway binding

--- react/script.js
+++ react/script.js
@@ -1,27 +1,72 @@
 
-var items = [
+var listofcars = [
     {
-        img:"img.jpg",
-        title: "Title",
+        img:"4.png",
+        title: "Car",
         description: "Description"
     },
     {
-        img:"img.jpg",
-        title: "Title",
+        img:"4.png",
+        title: "Car",
         description: "Description"
     },
     {
-        img:"img.jpg",
-        title: "Title",
+        img:"4.png",
+        title: "Car",
         description: "Description"
     },
     {
-        img:"img.jpg",
-        title: "Title",
+        img:"4.png",
+        title: "Car",
+        description: "Description"
+    },
+    {
+        img:"4.png",
+        title: "Car",
+        description: "Description"
+    },
+    {
+        img:"4.png",
+        title: "Car",
         description: "Description"
     }
 ]
+;
 
+
+var listofsweets = [
+    {
+        img:"img.jpg",
+        title: "Sweet",
+        description: "Description"
+    },
+    {
+        img:"img.jpg",
+        title: "Sweet",
+        description: "Description"
+    },
+    {
+        img:"img.jpg",
+        title: "Sweet",
+        description: "Description"
+    },
+    {
+        img:"img.jpg",
+        title: "Sweet",
+        description: "Description"
+    },
+    {
+        img:"img.jpg",
+        title: "Sweet",
+        description: "Description"
+    },
+    {
+        img:"img.jpg",
+        title: "Sweet",
+        description: "Description"
+    }
+]
+;
 
 
 class Card extends React.Component {
@@ -57,22 +102,130 @@ class Cards extends React.Component {
         super(props);
         this.cards = [];
 
-        items.forEach(item => {
-            this.cards.push(<Card img={item.img} title={item.title + " " + items.indexOf(item)} description={item.description} />)
+        props.list.forEach(item => {
+            this.cards.push(<Card img={item.img} title={item.title + " " + props.list.indexOf(item)} description={item.description} />)
         })
     }
 
     render () {
         return (
-            <div className="container">
+            
                 <div className="row">
                     {this.cards}                
                 </div>
-            </div>
+            
         )
     }
 }
 
+
+
+class SidebarItem extends React.Component {
+    render () {
+        return (
+            <a href={this.props.link} className="collection-item">{this.props.text}</a>
+        )
+    }
+}
+
+class Sidebar extends React.Component {
+    constructor (props) {
+        super(props);
+        this.items = [];
+
+        props.list.forEach(item => {
+            this.items.push(<SidebarItem link={"/"+item} text={item} />)
+        })
+    }
+
+    render () {
+        return (
+            <div class="collection">
+                {this.items}
+            </div>
+        );
+    }
+}
+
+
+var sidemenuitems = ["first", "second", "third", "fourth"];
+
+
+
+
+// ----------
+
+
+
+class Clock extends React.Component {
+    
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.changer(),
+            1000
+        );
+    }
+
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    changer() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+
+
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+  
+ 
+
+
+
+
+// ---------
+
+
+
+
+
+
+
+
+
 $(document).ready(()=>{
-    ReactDOM.render(<Cards />, document.getElementById("root"))
+    ReactDOM.render(
+        <div className="container">
+            <div className="row">
+                <div className="col s12">
+                    <div className="card-panel center">
+                        {true? <Clock /> : <lock></lock>}
+                    </div>
+                </div>
+                <div className="col s9">
+                    <Cards list={listofsweets} />
+                    <Cards list={listofcars} />
+                </div>
+                <div className="col s3">
+                    <Sidebar list={sidemenuitems} />
+                </div>
+            </div>
+            
+        </div>, document.getElementById("root"))
 })
